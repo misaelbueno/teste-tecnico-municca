7984aa9a7cd4721970f9959ed93b8e674110e7fc
Misael Bueno <misa.hbueno@gmail.com>
2024-10-08 11:56:54 -0300

    add api docs with swagger

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+
 import router from './routes.js'
+
+import swaggerDocs from './swagger.json' assert { type: 'json' };
 
 const app = express()
 
 app.use(express.json())
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
 app.use(router)
 
 app.listen(3333, () => console.log('Server is running'))
